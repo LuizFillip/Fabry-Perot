d2d1dec45c4c0cfcb4c609bb02b1cb357fb59e72
Luiz Fillip Rodrigues Vital <72110299+LuizFillip@users.noreply.github.com>
2023-02-05 17:18:43 -0300

    new funcs

--- plot/plotEffetiveWind.py
+++ plot/plotEffetiveWind.py
@@ -1,62 +1,59 @@
 import setup as s
 import pandas as pd
-import numpy as np
+from datetime import datetime, timedelta
 import matplotlib.pyplot as plt
-import matplotlib.dates as dates
-from wind_equations import CarrascoEquation, run_igrf
-from utils import datetime_to_float
-pd.options.mode.chained_assignment = None 
+from wind_equations import CarrascoEquation, PauloEquation, run_igrf
 
-infile = "database/HWM/cariri_winds_2013.txt"
-
-df = pd.read_csv(infile, index_col = "time")
-
-df.index = pd.to_datetime(df.index)
-
-#df = df.resample("1H").mean()
-
-d, i = run_igrf(year = 2013)
-
-u = CarrascoEquation(df.zon, df.mer, d, i).to_frame()
-
-def pivot_format(df, values = 0):
-    df = df.loc[(df.index.hour >= 20) |
-                 (df.index.hour <= 8)]
+def load(infile):
+    df = pd.read_csv(infile, index_col = "time")
     
-    df["time2"] = datetime_to_float(df)
-    df["day"] = df.index.date
+    try:
+        del df["Unnamed: 0"]
+    except:
+        pass
+    
+    df.index = pd.to_datetime(df.index)
+    return df
 
-    return pd.pivot_table(df,
-                          values = values, 
-                          index = "time2", 
-                          columns = "day")
+def compute_winds(df, func = CarrascoEquation):
+    d, i = run_igrf(year = 2013)
+
+    return func(df.zon, df.mer, d, i).to_frame(name = "u")
+
+def sel_interval(u, b):
+    e = b + timedelta(hours = 11)
+    return u.loc[(u.index >= b) & (u.index <= e)]
 
 
-u2 = pivot_format(u)
-
-fig, ax = plt.subplots(figsize = (10, 4))
-
-img = ax.contourf(u2.columns,
-            u2.index, 
-            u2.values, 60, cmap = "jet")
-
-
-label = "$(U_\\theta \cos D + U_\phi \sin D ) \sin I$"
-
-ax.set(title = label, 
-       xlabel = "Meses", 
-       ylabel = "Hora universal")
-
-vmin, vmax, step = -40, 20, 10
-s.colorbar_setting(
-    img, ax, 
-    ticks = np.arange(vmin, 
-                      vmax + step, 
-                      step), 
-    label = "Velocidade (m/s)"
-    )
-
-ax.xaxis.set_major_formatter(dates.DateFormatter('%b'))
-ax.xaxis.set_major_locator(dates.MonthLocator(interval = 1))
-#ax.legend()
- 
+def main():
+    
+    infile = "database/HWM/cariri_winds_2013.txt"
+    
+    
+    df = load(infile)
+    
+    b = datetime(2013, 1, 1, 21, 0)
+    
+    args = dict(lw = 2)
+    u = sel_interval(
+        compute_winds(df, func = PauloEquation), b
+        )
+    
+    fig, ax = plt.subplots(figsize = (8, 4))
+    
+    ax.plot(u, **args,
+            label = "$(U_\phi \cos D + U_\\theta \sin D ) \cos I$")
+    
+    u2 = sel_interval(
+        compute_winds(df, func = CarrascoEquation), b
+        )
+    
+    ax.plot(u2, **args,
+            label = "$U_\\theta \cos D + U_\phi \sin D$")
+    
+    ax.set(ylabel = "Velocidade (m/s)", 
+           xlabel = "Hora universal (UT)")
+    
+    ax.legend()
+    
+    s.format_axes_date(ax, time_scale="hour", interval = 2)
