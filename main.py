3a81a0a1c5917f6f23837fba536f72d80f49dd72
Luiz Fillip Rodrigues Vital <72110299+LuizFillip@users.noreply.github.com>
2023-02-26 16:54:19 -0300

    run mean

--- main.py
+++ main.py
@@ -3,22 +3,21 @@ from FabryPerot.utils import file_attrs
 from build import paths as p
 import os
 from FabryPerot.core import FabryPerot
-from FabryPerot.base import concat_directions
+from FabryPerot.base import concat_directions, get_monthly_mean
 import time
 
 def run_year(year = 2013):
     
     year = str(year)
     
-    files = p("FabryPerot")
+    f = p("FabryPerot")
 
     out = []
     
-    for filename in files.get_files_in_dir(year):
-        
-        f = os.path.split(filename)[-1]
-        
-        print("processing...", file_attrs(f).date)
+    for filename in f.get_files_in_dir(year):
+                
+        print("processing...", 
+              file_attrs(os.path.split(filename)[-1]).date)
         try:
             out.append(concat_directions(
                 FabryPerot(filename).wind))
@@ -27,18 +26,24 @@ def run_year(year = 2013):
      
     df = pd.concat(out)
     
-    save = os.path.join(p("FabryPerot").root, 
-                        "PRO", 
+    save = os.path.join(f.get_dir("PRO"), 
                         f"{year}.txt")
     
     df.to_csv(save, index = True)
     
     return df
 
-start = time.time()
 
+def run_monthly_mean():
+    for coord in ["zon", "mer"]:
+        get_monthly_mean(Dir = coord)
 
-run_year()
+def main():
+    start = time.time()
+    
+    run_year()
+    run_monthly_mean()
 
-
-print("%s" % (time.time() - start))
+    print("%s" % (time.time() - start))
+    
+main()
