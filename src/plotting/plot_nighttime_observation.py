b721a89468fab771f127ca407b606164ddec8159
Luiz Fillip Rodrigues Vital <72110299+LuizFillip@users.noreply.github.com>
2023-05-22 10:38:01 -0300

    new layout

--- src/plotting/plot_nighttime_observation.py
+++ src/plotting/plot_nighttime_observation.py
@@ -1,117 +1,104 @@
 import matplotlib.pyplot as plt
-from FabryPerot import FabryPerot
-from FabryPerot.src.base import running_avg
+import FabryPerot as fp
+import os
+import settings as s
 
 
-
-
-
-def plotErrobar(ax, 
-                df, 
-                up, 
-                par = "vnu"):
-    
-    label = up.title()
-    
-    args = dict(capsize = 2, lw = 1.5)
-    
-    zon = df.loc[(df["dir"] == up)]
-
-    ax.errorbar(zon.index, 
-                zon[par], 
-                yerr = zon[f"d{par}"], 
-                **args, 
-                label = label)
-    
-    ax.legend()
-    
-    ax.grid()
-    
-    
-def plot_average(ax, df, di, sample = "10min", 
-            marker = "s",
-            color = "k"):
+def plot_average(
+        ax, df, di, 
+        sample = "10min", 
+        marker = "s",
+        color = "k"):
     
     args = dict(marker = marker,
-                color = color, 
                 lw = 2, 
                 fillstyle = "none")
 
-    avg30 = running_avg(df, Dir = di[:3])
+    avg30 = fp.running_avg(df, Dir = di)
     
-    ax.plot(avg30, 
-            **args,
+    ax.plot(avg30, **args,
             label = f"Média ({sample})")
     
-    ax.legend()
     
     
-def plot_attrs(ax, df):
-    names = ["zonal", "meridional"]
-    for i, ax in enumerate(ax.flat):
-        
-        di = names[i]
-        
-        plot_average(ax, df, di)
-          
-        ax.set(title =  f"Vento {di}")
-        ax.axhline(0, color = "r", linestyle = "--")
 
-def plot_nighttime_observation(infile, 
-                               Type = "wind"):
+def plot_directions(ax,
+        path, parameter = "vnu"
+        ):
     
-    if Type == "wind":
-        df = FabryPerot(infile).wind
+    if parameter == "vnu":
+        df = fp.FPI(path).wind
     else:
-        df = FabryPerot(infile).temp
+        df = fp.FPI(path).temp
         
-    fig, ax = plt.subplots(ncols = 2, 
-                           figsize = (14, 4), 
+    coords = {
+        "zon": ("east", "west"), 
+        "mer": ("north", "south")
+        }
+    
+    names = ["zonal", "meridional"]
+    
+    for i, coord in enumerate(coords.keys()):
+        
+        plot_average(
+               ax[i], df, coord, 
+               sample = "10min", 
+               marker = "s",
+               color = "k"
+               )
+        for direction in coords[coord]:
+            
+            ds = df.loc[(df["dir"] == direction)]
+            
+            ax[i].errorbar(
+                ds.index, 
+                ds[parameter], 
+                yerr = ds[f"d{parameter}"], 
+                label = direction
+                )
+        ax[i].legend(loc = "upper right", ncols = 3)
+        ax[i].set(ylabel = f"Vento {names[i]} (m/s)")
+        ax[i].axhline(0, color = "k", linestyle = "--")
+
+
+
+
+def plot_nighttime_observation(
+        path, 
+        parameter = "vnu"
+        ):
+    
+        
+    fig, ax = plt.subplots(nrows = 2, 
+                           figsize = (8, 8), 
                            sharex = True, 
-                           sharey = True)
+                           sharey = True, 
+                           dpi = 300)
     
-    plt.subplots_adjust(wspace = 0.05)
+    plt.subplots_adjust(hspace = 0.05)
     
-    s.config_labels(fontsize = 15)
     
-    coor = {"zon": ("east", "west"), 
-            "mer": ("north", "south")}
+    plot_directions(ax, path, parameter = parameter)
     
-    for up, down in zip(coor["zon"], coor["mer"]):
-        
-        plotErrobar(ax[0], df, up)
-       
-        plotErrobar(ax[1], df, down)
-      
-        
-    ax[0].set(ylabel = "Velocidade (m/s)")
+    s.format_time_axes(
+            ax[1], hour_locator = 1, 
+            day_locator = 1, 
+            tz = "UTC"
+            )
+
     
-    s.format_axes_date(ax[0], time_scale = "hour", 
-                       interval = 2)
-    
-    fig.text(0.85, -0.15, "Hora universal (UT)", 
-             transform = ax[0].transAxes)
-    
-    date = df.index[0].strftime("%d/%m/%Y")
-    fig.suptitle(f"Cariri - {date}")
-    
-    plot_attrs(ax, df)
-    
-    plt.show()
-    
+    ax[0].set_title("Cariri")
+
     return fig, ax 
         
 def main():
+    
+    infile = "database/FabryPerot/2013/"
+    files = os.listdir(infile)
+
+    filename = files[2]
         
-    
-    infile = files[1]
-    
-    plot_nighttime_observation(infile)
+    plot_nighttime_observation(os.path.join(infile, filename))
 
-# main()
+main()
 
-import os
-
-files = os.listdir("database/FabryPerot/2013/")
-
-files
