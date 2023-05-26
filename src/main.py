8a09b2796a826fadabca3145c0e9e1c52160823d
Luiz Fillip Rodrigues Vital <72110299+LuizFillip@users.noreply.github.com>
2023-05-26 11:38:46 -0300

    process whole folder by year

--- src/main.py
+++ src/main.py
@@ -1,48 +1,53 @@
 import pandas as pd
-from build import paths as p
 import os
-from FabryPerot.src.core import FabryPerot
-from FabryPerot.src.utils import  date_from_filename
-from FabryPerot.src.base import concat_directions, get_monthly_mean
-import time
+import FabryPerot as fp
+from tqdm import tqdm
 
-def run_year(year = 2013):
+
     
-    year = str(year)
+def process_year(
+        infile, 
+        freq = "2min", 
+        parameter = "vnu",
+        year = 2013, site = "car"
+        ):
     
-    f = p("FabryPerot")
+
+    files = os.listdir(infile)
 
     out = []
+    for filename in tqdm(files, desc = "run"):
+        
+        path = os.path.join(infile, filename)
+        dn = fp.date_from_filename(filename)
+
+        if dn.year == year:
+            try:
+                out.append(fp.process_day(
+                    path, 
+                    freq = freq, 
+                    parameter = parameter)
+                    )
+            except:
+                print(filename)
+                continue
+        
+            
+    df = pd.concat(out).sort_index()
     
-    for filename in f.get_files_in_dir(year):
-                
-        print("processing...", 
-              date_from_filename(os.path.split(filename)[-1]))
-        try:
-            out.append(concat_directions(
-                FabryPerot(filename).wind))
-        except Exception:
-            continue
-     
-    df = pd.concat(out)
+    df = df[~df.index.duplicated()]
     
-    save = os.path.join(f.get_dir("PRO"), 
-                        f"{year}.txt")
-    
-    df.to_csv(save, index = True)
+    df.to_csv(f"database/FabryPerot/{site}_{parameter}_{year}.txt")
     
     return df
 
 
-def run_monthly_mean():
-    for coord in ["zon", "mer"]:
-        get_monthly_mean(Dir = coord)
-
 def main():
-    start = time.time()
-    
-    run_year()
-    run_monthly_mean()
 
-    print("%s seconds" % (time.time() - start))
-    
+    infile = "database/FabryPerot/2013/"
+    process_year(
+            infile, 
+            freq = "2min", 
+            parameter = "vnu",
+            year = 2013
+            )
