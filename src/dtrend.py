0c5a7de3db8a82949a10b36f302c2b085212f43a
Luiz Fillip Rodrigues Vital <72110299+LuizFillip@users.noreply.github.com>
2023-10-16 16:29:26 -0300

    compute lomb scargle and find periods

---
+++ src/dtrend.py
@@ -0,0 +1,88 @@
+import pandas as pd
+import FabryPerot as fp
+from astropy.timeseries import LombScargle
+from scipy.signal import find_peaks
+
+
+path = 'database/FabryPerot/cariri/2013/'
+fname = 'minime01_car_20130101.cedar.005.txt'
+
+
+
+def peaks_periods(infile, col = 'tn'):
+    
+    if col == 'vnu':
+        df = fp.FPI(infile).wind
+    else:
+        df = fp.FPI(infile).temp
+        
+    out = {'south' : [], 'north': [], 
+           'east': [], 'west': []}
+    
+    
+    for j, sector in enumerate(out.keys()):
+        
+        ds = df.loc[df['dir'] == sector]
+        
+        avg = ds[col].rolling('1H').mean(center = True)
+        
+        ds['dtrend'] = ds[col] - avg
+        
+        y = ds['dtrend'].values
+        t = ds['time'].values 
+        
+        ls = LombScargle(t, y)
+        
+        frequency, power = ls.autopower(
+                minimum_frequency = 1 / 5,
+                maximum_frequency = 1 / 1.1,
+                samples_per_peak = 100
+                )
+            
+        period = 1 / frequency
+        
+        points = find_peaks(power, height = 0.01)
+        
+        for i in points[0]:
+            out[sector].append(period[i])
+    
+    return out
+
+
+
+def diff_values(valor1, valor2):  
+    return abs(valor1 - valor2)
+
+def list_cond(p1, p2, p3, p4):
+    
+    diff_list = [
+        diff_values(p1, p2),
+        diff_values(p1, p4),
+        diff_values(p2, p3),
+        diff_values(p3, p4)
+        ]
+
+    return [t for t in diff_list if t <= 0.15]
+    
+def check_similiarities(out):
+
+    dic = []
+                
+    for p1 in out['east']:
+        for p2 in out['west']:
+            for p3 in out['north']:
+                for p4 in out['south']:
+                    
+                    if len(list_cond(p1, p2, p3, p4)) == 4:
+                        
+                        dic.append([p1, p2, p3, p4])
+       
+    index = len(dic)
+    return pd.DataFrame(
+        dic, columns = list(out.keys())
+        )
+
+infile = path + fname
+out = peaks_periods(infile, col = 'tn')
+
+df = check_similiarities(out)
