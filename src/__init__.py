27e55920c16dc179afa861f73d5fe46cec1fe091
Luiz Fillip Rodrigues Vital <72110299+LuizFillip@users.noreply.github.com>
2024-02-24 20:58:42 -0300

    update ini

--- src/__init__.py
+++ src/__init__.py
@@ -1,4 +1,5 @@
 from .core import FPI, load_FPI
 from .base import process_directions, resample_and_interpol, new_index
-from .fpi_utils import fn2dn
+from .utils import fn2dn
 from .period_analysis import get_similar
+from .avg import join_days, DIRECTIONS
